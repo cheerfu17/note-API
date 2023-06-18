2b0bdf2d69169b95eafaef7673e1aeb0a3ad5cab
Vlad <demonhpru@gmail.com>
2023-06-18 19:54:17 +0500

    add: Validation for registration
    +--------------------------------------------------------+
    
    Added field validation for registration:
    
        + Nickname field cannot be empty
        + Password must be longer than 8 characters
    
    +--------------------------------------------------------+

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,14 +1,19 @@
 import authService from "../Services/authService.js";
+import { validationResult } from "express-validator";
 class authController{
     async sign_up(req, res){
         try{
+            const errors = validationResult(req);
+            if (!errors.isEmpty()){
+                return res.status(400).json({"error": "Registation error", errors});
+            }
             const {nick_name, password} = req.body;
             const user = await authService.sign_up(nick_name, password);
             console.log(user);
             res.json(user);
         }catch(e){
             console.log(e);
-            res.status(400).json({"error": "Login error"});
+            return res.status(400).json({"error": e.message});
         }
     }
     async sign_in(req, res){
@@ -16,7 +21,7 @@ class authController{
             
         }catch(e){
             console.log(e);
-            res.status(400).json({"error": "Registration error"});
+            res.status(400).json({"error": "Registration error", "message": e.message});
         }
     }
 }
--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import authController from "../Controllers/authController.js";
+import { check } from "express-validator";
 const userRouter = new Router();
 
 userRouter.post('/sign_in', authController.sign_in);
-userRouter.post('/sign_up', authController.sign_up);
+
+userRouter.post('/sign_up', [
+    check('nick_name', 'Nickname field cannot be empty').notEmpty(),
+    check('password', 'Password must be longer than 8 characters').isLength({min: 8})
+], authController.sign_up);
 
 export default userRouter;
