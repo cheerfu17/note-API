92923e2a64becbc4f998c0a7ae5e0743de113567
Vlad <demonhpru@gmail.com>
2023-06-22 13:05:04 +0500

    update router for notes

--- Routers/noteRouter.js
+++ Routers/noteRouter.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import noteController from '../Controllers/noteController.js'
+import {check} from 'express-validator';
 const noteRouter = new Router();
 
 noteRouter.get('/note', noteController.get);
 noteRouter.get('/note/:id', noteController.get_one)
-noteRouter.post('/note', noteController.create);
-noteRouter.patch('/note/:id', noteController.put);
+noteRouter.post('/note', [check('title', 'Title field cannot be empty').notEmpty()],
+ noteController.create);
+noteRouter.patch('/note/:id', check('title', 'Title field cannot be empty').notEmpty(),
+ noteController.put);
 noteRouter.delete('/note/:id', noteController.delete);
 
 export {noteRouter};
